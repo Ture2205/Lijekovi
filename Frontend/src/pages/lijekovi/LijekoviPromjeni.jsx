96061dd6f7a22628769da7b11e9d79d12188fd40
Mario Turček <150047286+Ture2205@users.noreply.github.com>
2024-03-19 17:13:11 +0100

    trebao sam dodati potrebne zavisnosti

--- Frontend/src/pages/lijekovi/LijekoviPromjeni.jsx
+++ Frontend/src/pages/lijekovi/LijekoviPromjeni.jsx
@@ -1,7 +1,15 @@
 import { Container, Table } from "react-bootstrap";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { RiArrowGoBackFill } from "react-icons/ri"
+import { RiArrowGoForwardFill } from "react-icons/ri";
+import { RoutesNames } from "../../constants";
+import RadnikService from "../../services/LijekovikService";
+import { useEffect, useState } from "react";
 
 export default function LijekoviPromjeni(){
-
+    const navigate = useNavigate();
+    const routeParams = useParams();
+    const [lijekovi,setLijekovi] = useState({});
 
     return (
 
