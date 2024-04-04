51ecd312f3ebd8cae90aec65de6731ec4040a938
Mario Turček <150047286+Ture2205@users.noreply.github.com>
2024-04-04 17:30:35 +0200

    slovo g umjesto f u httpService

--- Frontend/src/services/httpService.js
+++ Frontend/src/services/httpService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const httpService = axios.create({
-    baseURL: 'https://mario2205-001-site1.ftempurl.com/api/v1',
+    baseURL: 'https://mario2205-001-site1.gtempurl.com/api/v1',
     headers:{
         'Content-Type': 'application/json; charset=utf-8, access-control-allow-origin'
     }
